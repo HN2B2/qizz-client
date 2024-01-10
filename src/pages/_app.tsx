cb761ad6da763b2a086fe9cab385c8aaed4eb03a
Dao Xuan Quy <quydxhe176351@fpt.edu.vn>
2024-01-10 10:39:41 +0700

    Initial Project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css"
+import "@mantine/core/styles.css"
+import "@mantine/carousel/styles.css"
+import "@mantine/charts/styles.css"
+import "@mantine/dates/styles.css"
+import "@mantine/dropzone/styles.css"
+import "@mantine/notifications/styles.css"
+import "@mantine/tiptap/styles.css"
+import { createTheme, MantineProvider } from "@mantine/core"
+import { ModalsProvider } from "@mantine/modals"
+import { Notifications } from "@mantine/notifications"
+import type { AppProps } from "next/app"
+
+const theme = createTheme({})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <MantineProvider theme={theme}>
+            <ModalsProvider>
+                <Notifications />
+                <Component {...pageProps} />
+            </ModalsProvider>
+        </MantineProvider>
+    )
 }
