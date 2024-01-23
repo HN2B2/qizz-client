18c8754a5caa0241a0cdd983351081b0364445a2
quynhnthe <quynhnthe171282@fpt.edu.vn>
2024-01-23 18:46:06 +0700

    feat: create live-quiz page

---
+++ src/components/quiz/index.ts
@@ -0,0 +1,2 @@
+export { default as QuizInfo } from "./QuizInfo";
+export { default as QuizQuestions } from "./QuizQuestions";
---
+++ src/pages/quiz/[quiz_id].tsx
@@ -0,0 +1,176 @@
+import { UserLayout } from "@/components/layouts";
+
+import { QuizInfo } from "@/components/quiz";
+import QuizQuestions from "@/components/quiz/QuizQuestions";
+import Quiz from "@/types/quiz/Quiz";
+import Question from "@/types/question/Question";
+import QuizQuestion from "@/types/quiz/QuizQuestion";
+import { User, UserStats } from "@/types/user";
+
+import {
+  Button,
+  Container,
+  Grid,
+  Group,
+  Menu,
+  Paper,
+  Stack,
+  rem,
+} from "@mantine/core";
+import {
+  IconDeviceDesktopStar,
+  IconEdit,
+  IconMessageCircle,
+  IconSettings,
+  IconTriangleInvertedFilled,
+} from "@tabler/icons-react";
+import React, { useState } from "react";
+import { UserHistory } from "@/components/profile";
+import { useRouter } from "next/router";
+const mockQuiz: Quiz = {
+  quizId: 1,
+  mode: "Quiz",
+  name: "Test 1",
+  description: "ring",
+  featuredImage: "",
+  createdAt: "2022-10-10",
+  // modifiedAt: string;
+  // publicable: boolean;
+  // publicEditable: boolean;
+  category: "test",
+  subcategory: "Math",
+  // quizBank: QuizBank,
+  bankId: 1,
+  totalQuestions: 10,
+  totalJoins: 10,
+  createBy: "Quynh",
+};
+
+const mockQuestion: Question = {
+  question_id: 1,
+  content:
+    "ringdkfkeruhgfoiuehfowiehfoewjgehgoehgeigoergherihgdsgdsrgrihfgoewihfowiehgoiewhf",
+  point: 10,
+  duration: "10",
+  type: "text",
+  answers_metadata: "A. dsj",
+  correct_answers_metadata: "A. ajdj",
+  explain_answer: "ring",
+  created_at: "2022-10-10",
+  modified_at: "2022-10-10",
+  disabled: false,
+  bank_id: 1,
+};
+
+const mockQuizQuestion: QuizQuestion = {
+  quiz_question_id: 1,
+  point: 10,
+  explain_answer: "ring",
+  createdAt: "2022-10-10",
+  modifiedAt: "2022-10-10",
+  quiz_id: 1,
+  question_id: 1,
+};
+
+const mockStats: UserStats = {
+  totalQuizzes: 0,
+  totalFavorites: 0,
+};
+
+// const router = useRouter();
+// const handleLiveQuiz = () => {
+//   router.push("/profile");
+// };
+const StartQuizPage = () => {
+  const [quizzes, setQuizzes] = useState(mockQuiz);
+  const [quizQuestion, setQuizQuestion] = useState(mockQuizQuestion);
+  const [question, setQuestion] = useState(mockQuestion);
+
+  return (
+    <UserLayout>
+      <Grid gutter={{ base: 5, xs: "md", md: "xl", xl: "xl" }}>
+        <Grid.Col span={{ base: 12, md: 8, lg: 8, xs: 7 }}>
+          <Container size="xl">
+            <QuizInfo quiz={quizzes} />
+            <Paper p="lg" radius="md" shadow="sm">
+              <Stack justify="space-between">
+                <Group gap="md" justify="center">
+                  <Menu shadow="md" width="22%">
+                    <Menu.Target>
+                      <Button
+                        variant="gradient"
+                        gradient={{
+                          from: "blue",
+                          to: "cyan",
+                          deg: 90,
+                        }}
+                        size="xl"
+                        style={{ width: "45%" }}
+                        p={0}
+                      >
+                        <Grid>
+                          <Grid.Col span={2}>
+                            <IconDeviceDesktopStar />
+                          </Grid.Col>
+                          <Grid.Col span={8}>Start Quiz</Grid.Col>
+                          <Grid.Col span={2} style={{ alignItems: "right" }}>
+                            <IconTriangleInvertedFilled size={10} />
+                          </Grid.Col>
+                        </Grid>
+                      </Button>
+                    </Menu.Target>
+
+                    <Menu.Dropdown>
+                      <Menu.Item
+                        leftSection={
+                          <IconSettings
+                            style={{ width: rem(14), height: rem(14) }}
+                          />
+                        }
+                        // onClick={handleLiveQuiz}
+                      >
+                        Live quiz
+                      </Menu.Item>
+
+                      <Menu.Divider />
+                      <Menu.Item
+                        leftSection={
+                          <IconMessageCircle
+                            style={{ width: rem(14), height: rem(14) }}
+                          />
+                        }
+                      >
+                        Test
+                      </Menu.Item>
+                    </Menu.Dropdown>
+                  </Menu>
+                  <Button
+                    variant="gradient"
+                    gradient={{
+                      from: "blue",
+                      to: "cyan",
+                      deg: 90,
+                    }}
+                    size="xl"
+                    style={{ width: "45%" }}
+                    leftSection={<IconEdit size={25} />}
+                  >
+                    Edit profile
+                  </Button>
+                </Group>
+              </Stack>
+            </Paper>
+            <QuizQuestions
+              quizQuestion={quizQuestion}
+              question={question}
+              quiz={quizzes}
+            />
+          </Container>
+        </Grid.Col>
+        <Grid.Col span={{ base: 12, md: 4, lg: 4, xs: 5 }}></Grid.Col>
+      </Grid>
+    </UserLayout>
+  );
+};
+
+export default StartQuizPage;
