4527cad0bce78838f68537ff746f5372bf5f06d2
quynhnthe <quynhnthe171282@fpt.edu.vn>
2024-01-30 15:22:08 +0700

    feat: create UserQuizzes page

--- src/components/profile/UserQuizzes/index.tsx
+++ src/components/profile/UserQuizzes/index.tsx
@@ -1,7 +1,52 @@
-import React from "react"
-
-const UserQuizzes = () => {
-    return <div>UserQuizzes</div>
+import { QuizBank } from "@/types/quizBank";
+import {
+  Avatar,
+  Card,
+  Flex,
+  Grid,
+  Group,
+  Paper,
+  Stack,
+  Text,
+} from "@mantine/core";
+import React from "react";
+interface QuizzesProps {
+  quizzes: QuizBank[];
 }
 
-export default UserQuizzes
+const UserQuizzes = (quizzes: QuizzesProps) => {
+  return (
+    <Grid>
+      <Grid.Col span={4}>
+        <Stack gap={10}>
+          <Paper p="sm" radius="md" shadow="sm" mb="md">
+            <Text fw={500}>2 results</Text>
+          </Paper>
+          <Card p="xl" radius="md" shadow="sm" mb="md" component="a" href="">
+            {/* <Paper p="sm" radius="md" shadow="sm" mb="md"> */}
+            <Card.Section>
+              <Group>
+                <Avatar src={quizzes.quizzes[0].featuredImage} radius={"sm"} />
+                <Stack gap={2}>
+                  <Text fw={500} size="lg">
+                    {quizzes.quizzes[0].name}
+                  </Text>
+                  <Flex justify="space-between">
+                    <Text size="sm">Question</Text>
+                  </Flex>
+                </Stack>
+              </Group>
+            </Card.Section>
+
+            {/* </Paper> */}
+          </Card>
+        </Stack>
+      </Grid.Col>
+      <Grid.Col span={8}>
+        <Paper p="lg" radius="md" shadow="sm" mb="md"></Paper>
+      </Grid.Col>
+    </Grid>
+  );
+};
+
+export default UserQuizzes;
---
+++ src/pages/profile/[username].tsx
@@ -0,0 +1,77 @@
+import { UserLayout } from "@/components/layouts";
+import {
+  UserFavorites,
+  UserHistory,
+  UserProfile,
+  UserQuizzes,
+} from "@/components/profile";
+import { QuizBank } from "@/types/quizBank";
+import { UserResponse, UserStats } from "@/types/user";
+import { UserRole } from "@/types/user/UserResponse";
+import { Container, Paper, Tabs } from "@mantine/core";
+import { useState } from "react";
+
+const mockUser: UserResponse = {
+  id: 1,
+  displayName: "QuynhNt",
+  username: "quynhNt",
+  email: "abc@qizz.tech",
+  role: UserRole.USER,
+  createdAt: "2021-08-01T00:00:00.000Z",
+  modifiedAt: "2021-08-01T00:00:00.000Z",
+};
+
+const mockStats: UserStats = {
+  totalQuizzes: 0,
+  totalFavorites: 0,
+};
+
+const mockQuizzes: QuizBank[] = [
+  {
+    id: 1,
+    name: "Quiz 1",
+    description: "Quiz 1 description",
+    featuredImage: "",
+    createdAt: "2021-08-01T00:00:00.000Z",
+    modifiedAt: "2021-08-01T00:00:00.000Z",
+    publicable: true,
+    publicEditable: false,
+  },
+];
+
+const UserProfilePage = () => {
+  const [user, setUser] = useState(mockUser);
+  const [stats, setStats] = useState(mockStats);
+  const [quizzes, setQuizzes] = useState(mockQuizzes);
+
+  return (
+    <UserLayout>
+      <Container size="xl">
+        <UserProfile user={user} stats={stats} />
+        {/* <Paper p="lg" radius="md" shadow="sm"> */}
+        <Tabs radius="md" defaultValue="quizzes">
+          <Tabs.List mb="md">
+            <Tabs.Tab value="quizzes">Quizzes</Tabs.Tab>
+            <Tabs.Tab value="favorites">Favorites</Tabs.Tab>
+            <Tabs.Tab value="history">History</Tabs.Tab>
+          </Tabs.List>
+
+          <Tabs.Panel value="quizzes">
+            <UserQuizzes quizzes={quizzes} />
+          </Tabs.Panel>
+
+          <Tabs.Panel value="favorites">
+            <UserFavorites />
+          </Tabs.Panel>
+
+          <Tabs.Panel value="history">
+            <UserHistory />
+          </Tabs.Panel>
+        </Tabs>
+        {/* </Paper> */}
+      </Container>
+    </UserLayout>
+  );
+};
+
+export default UserProfilePage;
