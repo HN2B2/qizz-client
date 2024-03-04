381c9f2cf5427be3d504b93b8a2ce702d9f96513
Thang Ngo <ngosythang123456@gmail.com>
2024-03-05 02:18:25 +0700

    fix ui setting quiz

--- src/pages/quiz/[id]/live_quiz.tsx
+++ src/pages/quiz/[id]/live_quiz.tsx
@@ -1,282 +1,235 @@
-import { UserLayout } from "@/components/layouts"
-import { instance } from "@/utils"
+import { UserLayout } from "@/components/layouts";
+import { instance } from "@/utils";
 import {
-    Button,
-    Container,
-    Divider,
-    Flex,
-    Group,
-    Input,
-    Paper,
-    Stack,
-    Switch,
-    Text,
-} from "@mantine/core"
-import { DateInput } from "@mantine/dates"
-import { IconChevronDown } from "@tabler/icons-react"
-import { useRouter } from "next/router"
-import React, { useState } from "react"
-const hour: number = new Date().getHours()
+  Button,
+  Container,
+  Divider,
+  Flex,
+  Group,
+  Input,
+  Paper,
+  Select,
+  Stack,
+  Switch,
+  Text,
+} from "@mantine/core";
+import { DateInput, DateTimePicker } from "@mantine/dates";
+import { IconChevronDown } from "@tabler/icons-react";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import { MdDateRange } from "react-icons/md";
+import dayjs from "dayjs";
+
+const hour: number = new Date().getHours();
 const LiveQuiz = () => {
-    const router = useRouter()
+  const router = useRouter();
 
-    const [value, setValue] = useState<Date | null>(null)
-    const clickOnSwitch = () => {
-        return (
-            <DateInput
-                valueFormat="YYYY MMM DD"
-                // label="Date input"
-                placeholder="Date input"
-            />
-        )
-    }
-    const [checked, setChecked] = useState(false)
-
-    // const router = useRouter();
-    const handleCreateQuiz = async () => {
-        try {
-            const { data } = await instance.post(
-                `/quiz/bankId/${router.query.id}`,
-                {
-                    quizName: "Untitled quiz",
-                    description: "hehe",
-                    featuredImage: "hoho",
-                }
-            )
-
-            const { data: quizData } = await instance.put(
-                `quiz/live_quiz/${data.quizId}`,
-                {
-                    metadata: [
-                        {
-                            key: "setTime",
-                            value: "2012-04-23",
-                        },
-                        {
-                            key: "attempt",
-                            value: "10",
-                        },
-                        {
-                            key: "showAnswersDuringAct",
-                            value: "true",
-                        },
-                        {
-                            key: "showAnswersAfterAct",
-                            value: "false",
-                        },
-                        {
-                            key: "shuffleQuestions",
-                            value: "true",
-                        },
-                        {
-                            key: "shuffleAnswers",
-                            value: "true",
-                        },
-                        {
-                            key: "skipQuestion",
-                            value: "true",
-                        },
-                        {
-                            key: "powerUp",
-                            value: "true",
-                        },
-                        {
-                            key: "reactions",
-                            value: "true",
-                        },
-                        {
-                            key: "showLeaderboard",
-                            value: "true",
-                        },
-                    ],
-                }
-            )
-            router.push(`/monitor/${data.code}`)
-        } catch (error) {
-            console.log(error)
-        }
-    }
+  const [value, setValue] = useState<Date | null>(null);
+  const clickOnSwitch = () => {
     return (
-        <UserLayout>
-            <Container size="xs">
-                <Paper p="lg" radius="md" shadow="sm">
-                    <Text style={{ fontWeight: "bold" }}>Set up quizzes</Text>
-                    <Divider my="md" />
-                    <Stack>
-                        <Flex justify={"space-between"}>
-                            <Text style={{ fontWeight: "500" }}>
-                                Set a start time for the activity
-                            </Text>
-                            <Switch
-                                checked={checked}
-                                onChange={() => setChecked(!checked)}
-                            />
-                        </Flex>
-                        <Flex justify={"space-between"}>
-                            {checked ? clickOnSwitch() : null}
-                        </Flex>
-                    </Stack>
+      <DateTimePicker
+        leftSection={<MdDateRange />}
+        clearable
+        defaultValue={new Date()}
+        placeholder="Pick date and time"
+        minDate={new Date()}
+        maxDate={dayjs(new Date()).add(1, "month").toDate()}
+      />
+    );
+  };
+  const [checked, setChecked] = useState(false);
 
-                    <Divider my="md" />
+  // const router = useRouter();
+  const handleCreateQuiz = async () => {
+    try {
+      const { data } = await instance.post(`/quiz/bankId/${router.query.id}`, {
+        quizName: "Untitled quiz",
+        description: "hehe",
+        featuredImage: "hoho",
+      });
 
-                    <Flex justify={"space-between"}>
-                        <Text style={{ fontWeight: "500" }}>
-                            Participant attempts
-                        </Text>
-                    </Flex>
-                </Paper>
+      const { data: quizData } = await instance.put(
+        `quiz/live_quiz/${data.quizId}`,
+        {
+          metadata: [
+            {
+              key: "setTime",
+              value: "2012-04-23",
+            },
+            {
+              key: "attempt",
+              value: "10",
+            },
+            {
+              key: "showAnswersDuringAct",
+              value: "true",
+            },
+            {
+              key: "showAnswersAfterAct",
+              value: "false",
+            },
+            {
+              key: "shuffleQuestions",
+              value: "true",
+            },
+            {
+              key: "shuffleAnswers",
+              value: "true",
+            },
+            {
+              key: "skipQuestion",
+              value: "true",
+            },
+            {
+              key: "powerUp",
+              value: "true",
+            },
+            {
+              key: "reactions",
+              value: "true",
+            },
+            {
+              key: "showLeaderboard",
+              value: "true",
+            },
+          ],
+        }
+      );
+      router.push(`/monitor/${data.code}`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <UserLayout>
+      <Container size="xs">
+        <Paper p="lg" radius="md" shadow="sm">
+          <Text style={{ fontWeight: "bold" }}>Set up quizzes</Text>
+          <Divider my="md" />
+          <Stack>
+            <Flex justify={"space-between"}>
+              <Text style={{ fontWeight: "500" }}>
+                Set a start time for the activity
+              </Text>
+              <Switch checked={checked} onChange={() => setChecked(!checked)} />
+            </Flex>
+            <Flex justify={"space-between"}>
+              {checked ? clickOnSwitch() : null}
+            </Flex>
+          </Stack>
 
-                <Paper p="lg" radius="md" shadow="sm" mt={20}>
-                    <Text style={{ fontWeight: "bold" }}>
-                        Question and Answer
-                    </Text>
-                    <Divider my="md" />
-                    <Stack>
-                        <Flex justify={"space-between"} gap={20} align="start">
-                            <Stack gap={5}>
-                                <Text style={{ fontWeight: "500" }}>
-                                    Show answers during activity
-                                </Text>
-                                <Text size="sm">
-                                    Show students the correct answers after each
-                                    question.
-                                </Text>
-                            </Stack>
+          <Divider my="md" />
 
-                            <Input
-                                component="select"
-                                rightSection={
-                                    <IconChevronDown size={14} stroke={1.5} />
-                                }
-                                pointer
-                                size="sm"
-                            >
-                                <option value="1">On</option>
-                                <option value="2">Validate only</option>
-                                <option value="3">Off</option>
-                            </Input>
-                        </Flex>
-                    </Stack>
+          <Flex justify={"space-between"}>
+            <Text style={{ fontWeight: "500" }}>Participant attempts</Text>
+            <Select
+              defaultValue={"Unlimited"}
+              data={["Unlimited", "1", "2", "3", "4", "5"]}
+            />
+          </Flex>
+        </Paper>
 
-                    <Divider my="md" />
+        <Paper p="lg" radius="md" shadow="sm" mt={20}>
+          <Text style={{ fontWeight: "bold" }}>Question and Answer</Text>
+          <Divider my="md" />
+          <Stack>
+            <Flex justify={"space-between"} gap={20} align="start">
+              <Stack gap={5}>
+                <Text style={{ fontWeight: "500" }}>
+                  Show answers during activity
+                </Text>
+                <Text size="sm">
+                  Show students the correct answers after each question.
+                </Text>
+              </Stack>
+              <Switch />
+            </Flex>
+          </Stack>
 
-                    <Flex justify={"space-between"} gap={20} align="start">
-                        <Stack gap={5}>
-                            <Text style={{ fontWeight: "500" }}>
-                                Show answers after activity
-                            </Text>
-                            <Text size="sm">
-                                Allow students to view answers after the quiz is
-                                submitted.
-                            </Text>
-                        </Stack>
+          <Divider my="md" />
 
-                        <Input
-                            component="select"
-                            rightSection={
-                                <IconChevronDown size={14} stroke={1.5} />
-                            }
-                            pointer
-                            size="sm"
-                        >
-                            <option value="1">On</option>
-                            <option value="2">Question only</option>
-                            <option value="3">Off</option>
-                        </Input>
-                    </Flex>
+          <Flex justify={"space-between"} gap={20} align="start">
+            <Stack gap={5}>
+              <Text style={{ fontWeight: "500" }}>
+                Show answers after activity
+              </Text>
+              <Text size="sm">
+                Allow students to view answers after the quiz is submitted.
+              </Text>
+            </Stack>
+            <Switch />
+          </Flex>
 
-                    <Divider my="md" />
-                    <Flex justify={"space-between"}>
-                        <Text style={{ fontWeight: "500" }}>
-                            Shuffle questions
-                        </Text>
-                        <Switch />
-                    </Flex>
+          <Divider my="md" />
+          <Flex justify={"space-between"}>
+            <Text style={{ fontWeight: "500" }}>Shuffle questions</Text>
+            <Switch />
+          </Flex>
 
-                    <Divider my="md" />
+          <Divider my="md" />
 
-                    <Flex justify={"space-between"}>
-                        <Text style={{ fontWeight: "500" }}>
-                            Shuffle answer options
-                        </Text>
-                        <Switch />
-                    </Flex>
+          <Flex justify={"space-between"}>
+            <Text style={{ fontWeight: "500" }}>Shuffle answer options</Text>
+            <Switch />
+          </Flex>
 
-                    <Divider my="md" />
+          <Divider my="md" />
 
-                    <Flex justify={"space-between"} gap={20} align="start">
-                        <Stack gap={5}>
-                            <Text style={{ fontWeight: "500" }}>
-                                Question Timer
-                            </Text>
-                            <Text size="sm">
-                                Students see a countdown and get extra points
-                                for each question
-                            </Text>
-                        </Stack>
+          <Flex justify={"space-between"}>
+            <Text style={{ fontWeight: "500" }}>
+              Skip Questions & Attempt Later
+            </Text>
+            <Switch />
+          </Flex>
+        </Paper>
 
-                        <Input
-                            component="select"
-                            rightSection={
-                                <IconChevronDown size={14} stroke={1.5} />
-                            }
-                            pointer
-                            size="sm"
-                        >
-                            <option value="1">Default timer</option>
-                            <option value="2">Off</option>
-                        </Input>
-                    </Flex>
+        <Paper p="lg" radius="md" shadow="sm" mt={20}>
+          <Text style={{ fontWeight: "bold" }}>Gamification</Text>
+          <Divider my="md" />
+          <Stack>
+            <Flex justify={"space-between"}>
+              <Stack gap={5}>
+                <Text style={{ fontWeight: "500" }}>Power-ups</Text>
+                <Text size="sm">
+                  Students get bonus points and other fun abilities.
+                </Text>
+              </Stack>
+              <Switch />
+            </Flex>
+          </Stack>
 
-                    <Divider my="md" />
+          <Divider my="md" />
+          <Stack>
+            <Flex justify={"space-between"}>
+              <Text style={{ fontWeight: "500" }}>Student live reactions</Text>
+              <Switch />
+            </Flex>
+          </Stack>
 
-                    <Flex justify={"space-between"}>
-                        <Text style={{ fontWeight: "500" }}>
-                            Skip Questions & Attempt Later
-                        </Text>
-                        <Switch />
-                    </Flex>
-                </Paper>
+          <Divider my="md" />
+          <Stack>
+            <Flex justify={"space-between"}>
+              <Text style={{ fontWeight: "500" }}>Show leaderboard</Text>
+              <Switch />
+            </Flex>
+          </Stack>
+        </Paper>
 
-                <Paper p="lg" radius="md" shadow="sm" mt={20}>
-                    <Text style={{ fontWeight: "bold" }}>Gamification</Text>
-                    <Divider my="md" />
-                    <Stack>
-                        <Flex justify={"space-between"}>
-                            <Stack gap={5}>
-                                <Text style={{ fontWeight: "500" }}>
-                                    Power-ups
-                                </Text>
-                                <Text size="sm">
-                                    Students get bonus points and other fun
-                                    abilities.
-                                </Text>
-                            </Stack>
-                            <Switch />
-                        </Flex>
-                        <Flex justify={"space-between"}>
-                            {checked ? clickOnSwitch() : null}
-                        </Flex>
-                    </Stack>
+        <Button
+          variant="gradient"
+          gradient={{ from: "blue", to: "cyan" }}
+          mt={20}
+          fullWidth
+          size="md"
+          onClick={() => {
+            handleCreateQuiz();
+          }}
+        >
+          Continue
+        </Button>
+      </Container>
+    </UserLayout>
+  );
+};
 
-                    <Divider my="md" />
-                </Paper>
-
-                <Button
-                    variant="gradient"
-                    gradient={{ from: "blue", to: "cyan" }}
-                    mt={20}
-                    fullWidth
-                    size="xl"
-                    onClick={() => {
-                        handleCreateQuiz()
-                    }}
-                >
-                    Continue
-                </Button>
-            </Container>
-        </UserLayout>
-    )
-}
-
-export default LiveQuiz
+export default LiveQuiz;
