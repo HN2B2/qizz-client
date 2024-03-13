c0c90d60a07f1f660f1a35802114e271094de608
quynhnthe <quynhnthe171282@fpt.edu.vn>
2024-03-13 11:58:55 +0700

    fix: report-question

--- src/pages/profile/[username].tsx
+++ src/pages/profile/[username].tsx
@@ -19,54 +19,54 @@ const mockStats: UserStats = {
   totalFavorites: 0,
 };
 
-const mockQuizzes: BankResponse[] = [
-  {
-    quizBankId: 1,
-    name: "Quiz 1",
-    description: "Quiz 1 description",
-    featuresImage: "",
-    createdAt: "2021-08-01T00:00:00.000Z",
-    modifiedAt: "2021-08-01T00:00:00.000Z",
-    quizPublicity: true,
-    publicEditable: true,
-    createdBy: {
-      id: 1,
-      username: "admin",
-      email: "admin@qizz",
-      role: UserRole.USER,
-      displayName: "Admin",
-      createdAt: "2021-08-01T00:00:00.000Z",
-      modifiedAt: "2021-08-01T00:00:00.000Z",
-      metadata: [],
-    },
-  },
-  {
-    quizBankId: 2,
-    name: "Quiz 1",
-    description: "Quiz 1 description",
-    featuresImage: "",
-    createdAt: "2021-08-01T00:00:00.000Z",
-    modifiedAt: "2021-08-01T00:00:00.000Z",
-    quizPublicity: true,
-    publicEditable: true,
-    createdBy: {
-      id: 2,
-      username: "admin",
-      email: "admin@qizz",
-      role: UserRole.USER,
-      displayName: "Admin",
-      createdAt: "2021-08-01T00:00:00.000Z",
-      modifiedAt: "2021-08-01T00:00:00.000Z",
-      metadata: [],
-    },
-  },
-];
+// const mockQuizzes: BankResponse[] = [
+//   {
+//     quizBankId: 1,
+//     name: "Quiz 1",
+//     description: "Quiz 1 description",
+//     featuresImage: "",
+//     createdAt: "2021-08-01T00:00:00.000Z",
+//     modifiedAt: "2021-08-01T00:00:00.000Z",
+//     quizPublicity: true,
+//     publicEditable: true,
+//     createdBy: {
+//       id: 1,
+//       username: "admin",
+//       email: "admin@qizz",
+//       role: UserRole.USER,
+//       displayName: "Admin",
+//       createdAt: "2021-08-01T00:00:00.000Z",
+//       modifiedAt: "2021-08-01T00:00:00.000Z",
+//       metadata: [],
+//     },
+//   },
+//   {
+//     quizBankId: 2,
+//     name: "Quiz 1",
+//     description: "Quiz 1 description",
+//     featuresImage: "",
+//     createdAt: "2021-08-01T00:00:00.000Z",
+//     modifiedAt: "2021-08-01T00:00:00.000Z",
+//     quizPublicity: true,
+//     publicEditable: true,
+//     createdBy: {
+//       id: 2,
+//       username: "admin",
+//       email: "admin@qizz",
+//       role: UserRole.USER,
+//       displayName: "Admin",
+//       createdAt: "2021-08-01T00:00:00.000Z",
+//       modifiedAt: "2021-08-01T00:00:00.000Z",
+//       metadata: [],
+//     },
+//   },
+// ];
 
 const UserProfilePage = () => {
   const { user } = useUser();
   // const [user, setUser] = useState(userData);
   const [stats, setStats] = useState(mockStats);
-  const [quizzes, setQuizzes] = useState(mockQuizzes);
+  // const [quizzes, setQuizzes] = useState(mockQuizzes);
 
   return (
     <UserLayout>
@@ -81,7 +81,7 @@ const UserProfilePage = () => {
             </Tabs.List>
 
             <Tabs.Panel value="quizzes">
-              <UserQuizzes quizzes={quizzes} />
+              {/* <UserQuizzes quizzes={quizzes} /> */}
               <></>
             </Tabs.Panel>
 
