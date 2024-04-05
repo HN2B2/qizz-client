37cb9a7d2b3cf9294ac9dc79c9a4609efbd0750d
Dao Xuan Quy <quydxhe176351@fpt.edu.vn>
2024-04-05 07:02:28 +0700

    fix: local url instance

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -12,6 +12,8 @@ import SubScript from "@tiptap/extension-subscript"
 import Image from "@tiptap/extension-image"
 import Youtube from "@tiptap/extension-youtube"
 
+export const appUrl = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000"
+
 export const instance = ky.create({
     prefixUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:6868/v1",
     credentials: "include",
@@ -41,7 +43,7 @@ export const getErrorStatusCode = (error: any) => {
 }
 
 export const localInstance = ky.create({
-    prefixUrl: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
+    prefixUrl: appUrl,
     credentials: "include",
 })
 
