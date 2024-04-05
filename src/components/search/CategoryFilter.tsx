d44671f75e392f0c26c5ccfcd64cdb997061abb3
Dao Xuan Quy <quydxhe176351@fpt.edu.vn>
2024-04-05 09:37:49 +0700

    fix: fetch category

--- src/components/search/CategoryFilter.tsx
+++ src/components/search/CategoryFilter.tsx
@@ -1,131 +1,141 @@
-import React, { useEffect, useState } from "react";
-import { Checkbox, Divider, ScrollArea, Title } from "@mantine/core";
-import { useListState } from "@mantine/hooks";
-import { Category } from "@/types/category";
-import { instance } from "@/utils";
-import { useRouter } from "next/router";
-import { PAGE_SIZE } from "@/pages/search";
+import React, { useEffect, useState } from "react"
+import { Checkbox, Divider, ScrollArea, Title } from "@mantine/core"
+import { useListState } from "@mantine/hooks"
+import { Category } from "@/types/category"
+import { instance } from "@/utils"
+import { useRouter } from "next/router"
+import { PAGE_SIZE } from "@/pages/search"
 
 interface CheckedState {
-  [key: string]: boolean;
+    [key: string]: boolean
 }
 
 interface Type {
-  label: string;
-  checked: boolean;
-  key: number;
+    label: string
+    checked: boolean
+    key: number
 }
 
 const CategoryFilter = () => {
-  const [checkedCategories, setCheckedCategories] = useState<CheckedState>({});
-  const [checkedSubCategories, setCheckedSubCategories] =
-    useState<CheckedState>({});
-  const [categories, handlerCategories] = useListState<Category>([]);
-  const [list, listHandler] = useListState<number>([]);
-  const [checkedCat, setCheckedCat] = useListState<number>([]);
-  const [checkedSub, setCheckedSub] = useListState<number>([]);
-  const [values, handlers] = useListState<Type>();
+    const [checkedCategories, setCheckedCategories] = useState<CheckedState>({})
+    const [checkedSubCategories, setCheckedSubCategories] =
+        useState<CheckedState>({})
+    const [categories, handlerCategories] = useListState<Category>([])
+    const [list, listHandler] = useListState<number>([])
+    const [checkedCat, setCheckedCat] = useListState<number>([])
+    const [checkedSub, setCheckedSub] = useListState<number>([])
+    const [values, handlers] = useListState<Type>()
 
-  const allChecked = values.every((value) => value.checked);
-  const indeterminate = values.some((value) => value.checked) && !allChecked;
-  const router = useRouter();
-  const { keyword, order, sort, page, subCategoryIds } = router.query;
-  interface CategoryResponse {
-    data: Category[];
-    total: number;
-  }
-  useEffect(() => {
-    const fetchCategories = async () => {
-      try {
-        const data: CategoryResponse = await instance.get("categories").json();
-
-        handlerCategories.setState(data.data);
-      } catch (error) {
-        console.error("Error fetching categories:", error);
-      }
-    };
-    fetchCategories();
-  }, []);
-
-  const handleCat = (value: number) => {
-    if (checkedCat.includes(value)) {
-      setCheckedCat.remove(checkedCat.indexOf(value));
-      let newS = checkedSub.filter(
-        (id) =>
-          !categories
-            .find((c) => c.id === value)
-            ?.subCategories.map((c) => c.id)
-            .includes(id)
-      );
-      setCheckedSub.setState(newS);
-    } else {
-      setCheckedCat.append(value);
-      categories
-        .find((category) => category.id === value)
-        ?.subCategories.forEach((subCategory) => {
-          if (!checkedSub.includes(subCategory.id)) {
-            setCheckedSub.append(subCategory.id);
-          }
-        });
+    const allChecked = values.every((value) => value.checked)
+    const indeterminate = values.some((value) => value.checked) && !allChecked
+    const router = useRouter()
+    const { keyword, order, sort, page, subCategoryIds } = router.query
+    interface CategoryResponse {
+        data: Category[]
+        total: number
     }
-  };
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const data: CategoryResponse = await instance
+                    .get("categories?limit=100")
+                    .json()
 
-  const handleSub = (value: number) => {
-    if (checkedSub.includes(value)) {
-      setCheckedSub.remove(checkedSub.indexOf(value));
-    } else {
-      setCheckedSub.append(value);
+                handlerCategories.setState(data.data)
+            } catch (error) {
+                console.error("Error fetching categories:", error)
+            }
+        }
+        fetchCategories()
+    }, [])
+
+    const handleCat = (value: number) => {
+        if (checkedCat.includes(value)) {
+            setCheckedCat.remove(checkedCat.indexOf(value))
+            let newS = checkedSub.filter(
+                (id) =>
+                    !categories
+                        .find((c) => c.id === value)
+                        ?.subCategories.map((c) => c.id)
+                        .includes(id)
+            )
+            setCheckedSub.setState(newS)
+        } else {
+            setCheckedCat.append(value)
+            categories
+                .find((category) => category.id === value)
+                ?.subCategories.forEach((subCategory) => {
+                    if (!checkedSub.includes(subCategory.id)) {
+                        setCheckedSub.append(subCategory.id)
+                    }
+                })
+        }
     }
-  };
 
-  useEffect(() => {
-    router.push({
-      pathname: "/search",
-      query: {
-        limit: PAGE_SIZE,
-        page,
-        keyword,
-        order,
-        sort,
-        subCategoryIds: checkedSub.join(","),
-      },
-    });
-  }, [checkedSub]);
+    const handleSub = (value: number) => {
+        if (checkedSub.includes(value)) {
+            setCheckedSub.remove(checkedSub.indexOf(value))
+        } else {
+            setCheckedSub.append(value)
+        }
+    }
 
-  return (
-    <>
-      <Title size="md">Category</Title>
-      <ScrollArea style={{ height: 200 }} w="100%">
-        {categories.map((category, index) => (
-          <React.Fragment key={index}>
-            <Checkbox
-              label={category.name}
-              checked={checkedCat.includes(category.id) ?? false}
-              onChange={() => handleCat(category.id)}
-              labelPosition="right"
-              mb={5}
-            />
-            {category.subCategories.length > 0 && (
-              <>
-                {category.subCategories.map((subCategory, subIndex) => (
-                  <Checkbox
-                    key={subIndex}
-                    label={subCategory.name}
-                    checked={checkedSub.includes(subCategory.id) ?? false}
-                    labelPosition="right"
-                    mb={5}
-                    ml="xs"
-                    onChange={() => handleSub(subCategory.id)}
-                  />
+    useEffect(() => {
+        router.push({
+            pathname: "/search",
+            query: {
+                limit: PAGE_SIZE,
+                page,
+                keyword,
+                order,
+                sort,
+                subCategoryIds: checkedSub.join(","),
+            },
+        })
+    }, [checkedSub])
+
+    return (
+        <>
+            <Title size="md">Category</Title>
+            <ScrollArea style={{ height: 200 }} w="100%">
+                {categories.map((category, index) => (
+                    <React.Fragment key={index}>
+                        <Checkbox
+                            label={category.name}
+                            checked={checkedCat.includes(category.id) ?? false}
+                            onChange={() => handleCat(category.id)}
+                            labelPosition="right"
+                            mb={5}
+                        />
+                        {category.subCategories.length > 0 && (
+                            <>
+                                {category.subCategories.map(
+                                    (subCategory, subIndex) => (
+                                        <Checkbox
+                                            key={subIndex}
+                                            label={subCategory.name}
+                                            checked={
+                                                checkedSub.includes(
+                                                    subCategory.id
+                                                ) ?? false
+                                            }
+                                            labelPosition="right"
+                                            mb={5}
+                                            ml="xs"
+                                            onChange={() =>
+                                                handleSub(subCategory.id)
+                                            }
+                                        />
+                                    )
+                                )}
+                            </>
+                        )}
+                        <Divider mt="xs" mb="sm" color={"gray"} />
+                    </React.Fragment>
                 ))}
-              </>
-            )}
-            <Divider mt="xs" mb="sm" color={"gray"} />
-          </React.Fragment>
-        ))}
-      </ScrollArea>
-    </>
-  );
-};
+            </ScrollArea>
+        </>
+    )
+}
 
-export default CategoryFilter;
+export default CategoryFilter
